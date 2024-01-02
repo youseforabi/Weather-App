aa200408bd7483c9327b682d26bdbe345b7588ff
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 01:45:27 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,4 @@
 
-let homePage = document.getElementById("homePage").addEventListener("click" , function(){
-    window.open("../index.html" , "_self");
-})
-
-let logo =document.querySelector(".logo").addEventListener("click" , function(){
-    window.open("../index.html" , "_self");
-})
-
-
-
 // the map
 function initMap() {
     let loc = { lat: 30.249406, lng: 31.361373 };
