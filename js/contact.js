0ddef134e2546a4d797f704febdee207c9a05832
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 14:30:55 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -3,15 +3,15 @@
 let map, infoWindow, geocoder;
 
 function initMap() {
- map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 30.397, lng: 31.644 },
     zoom: 6,
- });
- let marker = new google.maps.Marker({ position: loc, map: map });
- infoWindow = new google.maps.InfoWindow();
- geocoder = new google.maps.Geocoder();
+  });
+  let marker = new google.maps.Marker({ position: loc, map: map });
+  infoWindow = new google.maps.InfoWindow();
+  geocoder = new google.maps.Geocoder();
 
- if (navigator.geolocation) {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         let pos = {
@@ -23,24 +23,23 @@ function initMap() {
         infoWindow.setContent("Location found.");
         infoWindow.open(map);
         map.setCenter(pos);
-
       },
       () => {
         handleLocationError(true, infoWindow, map.getCenter());
       }
     );
- } else {
+  } else {
     handleLocationError(false, infoWindow, map.getCenter());
- }
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
- infoWindow.setPosition(pos);
- infoWindow.setContent(
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
     browserHasGeolocation
       ? "Error: The Geolocation service failed."
       : "Error: Your browser doesn't support geolocation."
- );
- infoWindow.open(map);
+  );
+  infoWindow.open(map);
 }
 
