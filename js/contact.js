6cb4b3c04f88316739f3aa0e3e7c2e84988c26d7
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 14:17:47 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -34,3 +34,13 @@ function initMap() {
  }
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+ infoWindow.setPosition(pos);
+ infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+ );
+ infoWindow.open(map);
+}
+
