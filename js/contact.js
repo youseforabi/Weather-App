a7c0735d7e2e3ca4f56122019983b340a5ddb474
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 14:06:43 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,45 +1,48 @@
 
 // the map
-let map, infoWindow, geocoder;
+let map, infoWindow, geocoder, marker;
 
 function initMap() {
- map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 30.397, lng: 31.644 },
-    zoom: 6,
- });
- let marker = new google.maps.Marker({ position: loc, map: map });
- infoWindow = new google.maps.InfoWindow();
- geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 30.397, lng: 31.644 },
+        zoom: 6,
+    });
+    infoWindow = new google.maps.InfoWindow();
+    geocoder = new google.maps.Geocoder();
 
- if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                let pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(pos);
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
 
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
- } else {
-    handleLocationError(false, infoWindow, map.getCenter());
- }
+                marker = new google.maps.Marker({ position: pos, map: map });
+
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
- infoWindow.setPosition(pos);
- infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
- );
- infoWindow.open(map);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 }
+
+window.initMap = initMap;
