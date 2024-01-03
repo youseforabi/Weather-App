15d83bb23f13117665eebaaae37e6c29e0091fa3
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 16:24:30 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,45 +1,11 @@
 
 // the map
-let map, infoWindow, geocoder;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 30.397, lng: 31.644 },
-    zoom: 6,
-  });
-  let marker = new google.maps.Marker({ position: loc, map: map });
-  infoWindow = new google.maps.InfoWindow();
-  geocoder = new google.maps.Geocoder();
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(pos);
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+let loc = { lat: 30.249406, lng: 31.361373 };
+let map = new google.maps.Map(
+document.getElementById("map"),
+{ zoom: 4, center: loc }
+);
+let marker = new google.maps.Marker({ position: loc, map: map });
 }
 
