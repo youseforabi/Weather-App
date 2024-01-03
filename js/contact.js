7ed3f6c89c7657f3386653e9e08787caae15419b
Yousef Orabi <102566557+youseforabi@users.noreply.github.com>
2024-01-03 13:43:45 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -4,7 +4,7 @@ let map, infoWindow, geocoder;
 
 function initMap() {
  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -134.397, lng: 150.644 },
     zoom: 6,
  });
  infoWindow = new google.maps.InfoWindow();
